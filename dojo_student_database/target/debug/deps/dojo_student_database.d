7f48b22a9237e7b1b6879501a51b29cc65268621
Logan Falkenberg <loganf0101@gmail.com>
2022-08-06 16:09:33 -0500

    Created cargo

---
+++ dojo_student_database/target/debug/deps/dojo_student_database.d
@@ -0,0 +1,5 @@
+C:\Users\Logan\PrivateContainer\rust\dojo_student_database\target\debug\deps\dojo_student_database.exe: src\main.rs
+
+C:\Users\Logan\PrivateContainer\rust\dojo_student_database\target\debug\deps\dojo_student_database.d: src\main.rs
+
+src\main.rs:
---
+++ dojo_student_database/target/debug/dojo_student_database.d
@@ -0,0 +1,1 @@
+C:\Users\Logan\PrivateContainer\rust\dojo_student_database\target\debug\dojo_student_database.exe: C:\Users\Logan\PrivateContainer\rust\dojo_student_database\src\main.rs
